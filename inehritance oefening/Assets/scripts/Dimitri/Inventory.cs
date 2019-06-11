d80340f26b4eaa6099bf07a60b1f88b710186270
Dimitri van Manen <vmanen68@gmail.com>
2019-06-11 22:11:55 +0200

    Added Subclasses for inventory definition

---
+++ inehritance oefening/Assets/scripts/Dimitri/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    public List<Item> items = new List<Item>();
+    private int space = 10;
+    public int Space { protected set { space = value; } get { return space; } }
+    public Action AddedItem;
+    public Action ItemHasBeenRemoved;
+
+    public bool isFull()
+    {
+        if(items.Count >= space)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void AddItem(Item newItem, int amount = 1)
+    {
+        if (items.Contains(newItem) && !isFull())
+        {
+            AddedItem?.Invoke();
+            items.Find(Item => newItem).AddToStack(amount);
+            return;
+        }
+        if (!isFull())
+        {
+            AddedItem?.Invoke();
+            newItem.AddToStack(amount);
+            items.Add(newItem);
+        }
+    }
+
+    public void RemoveItem(Item itemToRemove)
+    {
+        if (items.Contains(itemToRemove))
+        {
+            if(items.Find(Item => itemToRemove).stack == 1)
+            {
+                ItemHasBeenRemoved?.Invoke();
+                items.Remove(itemToRemove);
+                return;
+            }
+            ItemHasBeenRemoved?.Invoke();
+            items.Find(Item => itemToRemove).stack--;
+        }
+    }
+}
---
+++ inehritance oefening/Assets/scripts/Dimitri/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Item", menuName = "Inventory/Create new Item")]
+public class Item : ScriptableObject
+{
+    public Sprite icon;
+    public int stackLimit;
+    public int stack;
+    public string name;
+    public string description;
+    public bool dragable;
+
+    public void AddToStack(int amount)
+    {
+        if (stack + amount < stackLimit)
+            stack += amount;
+        else
+            Debug.LogWarning("It's to much items!");
+    }
+}
