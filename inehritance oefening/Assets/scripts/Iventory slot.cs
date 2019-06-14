7877512a196c2689d36181edda92ebcda5508abb
kubilay0 <49680603+2kubilay0@users.noreply.github.com>
2019-06-14 11:16:40 +0200

    sd

---
+++ inehritance oefening/Assets/scripts/Iventory slot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Iventoryslot : MonoBehaviour
+{
+    public ItemClass it;
+
+    public void Start()
+    {
+        
+    }
+
+     void Update()
+    {
+        
+    }
+
+}
